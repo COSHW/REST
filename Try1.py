71ff7ba08a9d5cba6dff6d9591c5459d1c0f29ae
COSHW <45237915+COSHW@users.noreply.github.com>
2018-11-21 22:11:37 +0300

    Update Try1.py

--- Try1.py
+++ Try1.py
@@ -4,9 +4,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/app/tools/create/<string:name>")
+@app.route("/<string:name>")
 def create_table(name):
-    print(name)
     return name
 
 
